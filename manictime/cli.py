7bc3581090520fd53e5173cddb44dd187d39ad63
Matthew <mkendon@gmail.com>
2014-09-10 09:21:37 +0100

    YTI-5 - refactored code

--- manictime/cli.py
+++ manictime/cli.py
@@ -3,12 +3,14 @@ import click
 from dateutil.parser import parse as date_parse
 import datetime
 from youtrack.connection import Connection
-import codecs
 import csv
 import re
 import youtrack as yt
 from manictime import ManicTimeRow
 
+
+class Messages(object):
+    pass
 
 @click.group()
 def youtrack():
@@ -31,7 +33,6 @@ def report(url, username, password, from_date_string, to_date_string):
     connection = get_connection(url, username, password)
     #get project ids and loop through them
     project_ids = connection.getProjectIds()
-    projects = []
     filter_string = "updated: " + from_date.strftime("%Y-%m-%d") + " .. " + to_date.strftime("%Y-%m-%d")
     report_items = []
     for id in project_ids:
@@ -43,7 +44,10 @@ def report(url, username, password, from_date_string, to_date_string):
         for issue in issues:
             work_items = connection.getWorkItems(issue.id)
             for item in work_items:
-                if item.authorLogin == username:
+                item_date = datetime.datetime.utcfromtimestamp(int(item.date)/1000)
+                if item.authorLogin == username \
+                        and item_date.strftime("%Y-%m-%d") >= from_date.strftime("%Y-%m-%d") \
+                        and item_date.strftime("%Y-%m-%d") <= to_date.strftime("%Y-%m-%d"):
                     report_items.append(item)
     report_items.sort(key=lambda work_item: work_item.date)
     previous_date = None
@@ -51,7 +55,7 @@ def report(url, username, password, from_date_string, to_date_string):
         item_date = datetime.datetime.utcfromtimestamp(int(item.date)/1000)
         current_date = item_date.strftime("%Y-%m-%d")
         if not current_date == previous_date:
-            print("\n\n")
+            print("\n")
             print(current_date)
             previous_date = current_date
         url_parts = item.url.split('/')
@@ -80,74 +84,81 @@ def manictime(url, username, password, filename):
     connection = get_connection(url, username, password)
 
     try:
-        # u = filename.decode('utf-8-sig')
-        # filename = u.encode('utf-8')
         rows = csv.DictReader(filename)
         print("Importing timeslips")
     except csv.Error as e:
-        exit("Cound not find file")
+        exit("Could not find file")
 
     count = 0
     total = 0
     for row in rows:
         total += 1
         row = ManicTimeRow(connection, row)
-        ignore = False
-
-        if re.search('ignore', row.get_tags(), re.IGNORECASE):
-            ignore = True
-        else:
-            first = True
-            while True:
-                # if we have issue id and issue exists break
-                if row.issue_exists():
-                    break
-                else:
-                    # if first time inform user what is going on
-                    if first:
-                        first = False
-                        response = input("  No Issue found for \"" + row.get_tags() + "\". Add to an issue? (y/n): ")
-                        if response.strip() == "n":
-                            ignore = True
-                            break
-                    # if no project is set prompt user to choose one.
-                    if not row.project_exists():
-                        message = "  Enter Project Id or Project Name for " + row.timeslip_string()
-                        message += " (Leave blank to skip this timeslip): "
-                        project = input(message)
-                        if project == "":
-                            ignore = True
-                            break
-                    else:
-                        message = "  Enter Issue Id or Search Terms for " + row.timeslip_string()
-                        message += " (Leave blank to skip this timeslip): "
-                        issue = input(message)
-                        if issue == "":
-                            ignore = True
-                            break
-                        results = row.search_issue(issue)
-                        if results.length > 1:
-                            pass
-                        elif results.length == 1:
-                            issue = results[0]
-                            correctIssue = input("    Found issue " + issue.ID)
-                        else:
-                            print("    No issue found with entered search: " + results + ". Please try again.")
-
-        if not ignore:
+        if process_row(row):
+            # save
             if not row.timeslip_exists():
-                    count += 1
-                    # row.save()
-                    print("    uploaded timeslip")
+                count += 1
+                # row.save()
+                print("    uploaded timeslip")
             else:
                 print("  Timeslip for " + row.get_issue_id() + " (" +
-                                 row.timeslip_string() + ") already exists")
+                      row.timeslip_string() + ") already exists")
         else:
+            # ignore
             print("  Timeslip ignored")
-
     print("Added " + str(count) + " timeslips out of " + str(total))
-    print("")
 
+def process_row(row):
+    # if ignore exists in tags, return False
+    if row.is_ignored():
+        return False
+    # if issue exists and we haven't ignored it
+    if row.issue_exists():
+        return True
+    # if issue does not exist lets prompt the user
+    response = input("  No Issue found for \"" + row.get_tags() + "\". Add to an issue? (y/n): ")
+    # if they respond "n" return False
+    if response.strip() == "n":
+        return False
+    # if they want to go ahead start loop
+    while True:
+        # check to see if issue exists
+        if row.issue_exists():
+            return True
+        # lets get a project from them
+        if not row.project_exists():
+            message = "  Enter Project Id for " + row.timeslip_string()
+            message += " (Leave blank to skip this timeslip): "
+            project_id = input(message)
+            # if left blank, ignore row
+            if project_id == "":
+                return False
+            # get the project from Youtrack
+            project = row.connection.getProject(project_id)
+            # if we have a project set it and continue
+            if isinstance(project, youtrack.Project):
+                row.project = project
+            # else tell the user and try again
+            else:
+                print("    Could not find project with " + project_id + ". Please try again.")
+                continue
+        # if we have a project lets try get an issue
+        else:
+            message = "  Enter Issue Id for " + row.timeslip_string()
+            message += " (Leave blank to skip this timeslip): "
+            issue_id = input(message)
+            # if left blank ignore row
+            if issue_id == "":
+                return False
+            # get the issue from Youtrack
+            issue = row.connection.get_issue(issue_id)
+            if isinstance(issue, youtrack.Issue):
+                row.issue = issue
+            else:
+                print("    Count not fin isue with id of " + issue_id + ". Please try again.")
+                continue
+    # if we ever get to ignore Row
+    return False
 
 def get_connection(url, username, password):
     try:
