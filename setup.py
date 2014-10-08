b59afeacb5935f507d55b5649d60804070b2ed1c
Matthew <mkendon@gmail.com>
2014-10-08 01:43:10 +0100

    added parsedatetime as requirement for nlp for dates

--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@ setup(
     install_requires=[
         'Click',
         'requests',
-        'python-dateutil'
+        'python-dateutil',
+        'parsedatetime'
     ],
     entry_points='''
         [console_scripts]
